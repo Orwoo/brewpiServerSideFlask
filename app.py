402ae6627520c8af02c69325de1ab215173d9c73
orwo <guenter.sens@uni-jena.de>
2023-09-07 17:03:33 +0200

    all working. first prod try

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from email.message import EmailMessage
-from flask import Flask, render_template, request, redirect, jsonify
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
 import json
 import smtplib
+from datetime import datetime
+from email.message import EmailMessage
 
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import BadRequestKeyError
-
-# from werkzeug.serving import run_simple
 
 # init app
 app = Flask(__name__)
@@ -20,6 +18,28 @@ db = SQLAlchemy(app)
 
 # Configure the limiter rate for all routes
 limiter = Limiter(get_remote_address, app=app,  default_limits=["5 per minute"])
+
+app.config['SECRET_KEY'] = 'your_secret_key'
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User()
+    user.id = user_id
+    return user
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    send_email(e)
+    return "Internal Server Error", 500
 
 
 # define db
@@ -47,6 +67,12 @@ class TempSet(db.Model):
     th_outer = db.Column(db.Float, nullable=False)
 
 
+class Creds(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(15), nullable=False)
+    pw = db.Column(db.String(25), nullable=False)
+
+
 def send_email(error):
     with open("./static/mail.json") as f:
         data = json.load(f)
@@ -64,37 +90,32 @@ def send_email(error):
         server.send_message(msg)
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    send_email(e)
-    return "Internal Server Error", 500
+@app.route("/")
+def root():
+    return redirect(url_for("login"))
 
 
-@app.route('/brewpi/')
+@app.route('/fermpi/')
+@login_required
 @limiter.limit("1 per second")  # custom call limit
-def hello_world():  # put application's code here
+def fermpi():  # put application's code here
     temps = Temperature.query.order_by(Temperature.timestamp.desc()).limit(10).all()
-
-    # set init temps if db is empty
-    if TempSet.query.count() == 0:
-        init_temps = TempSet(temp_set=18, th_set=1, th_outer=5)
-
-        try:
-            db.session.add(init_temps)
-            db.session.commit()
-        except Exception as e:
-            send_email(e)
-            return f"There was an issue initializing the set_temps"
-
     set_temps = TempSet.query.first()
     return render_template('index.html', temps=temps, set_temps=set_temps)
 
 
+# Route to get the parameters for fermentation from the server
+@app.route('/fermpi/get-set-temp', methods=['GET'])
+def get_set_temp():
+    temp_set_value = TempSet.query.first()
+    return f"{temp_set_value.temp_set},{temp_set_value.th_set},{temp_set_value.th_outer}".encode()
+
+
 # Route to which the client posts its present fermentation temps to the server
-@app.route('/brewpi/temp-client', methods=['POST'])
+@app.route('/fermpi/temp-client', methods=['POST'])
 def temp_client():
-    data = request.json
     try:
+        data = request.json
         temp = Temperature(temp_inner=data['temp_inner'], temp_outer=data['temp_outer'], temp_set=data['temp_set'])
         db.session.add(temp)
         db.session.commit()
@@ -103,16 +124,8 @@ def temp_client():
     return jsonify({'message': 'Temperature saved!'}), 200
 
 
-# Route to get the parameters for fermentation from the server
-@app.route('/brewpi/get-set-temp', methods=['GET'])
-def get_set_temp():
-    temp_set_value = TempSet.query.first()
-    # return jsonify({'temp_set': temp_set_value.temp_set}), 200
-    return f"{temp_set_value.temp_set},{temp_set_value.th_set},{temp_set_value.th_outer}".encode()
-
-
 # Route to update the parameters for fermentation in the server db
-@app.route('/brewpi/update-set-temp', methods=['POST'])
+@app.route('/fermpi/update-set-temp', methods=['POST'])
 def update_set_temp():
     try:
         update_temps = TempSet.query.first()
@@ -125,19 +138,35 @@ def update_set_temp():
 
     try:
         db.session.commit()
-        return redirect("/brewpi/")
+        return redirect("/fermpi/")
     except Exception as e:
         send_email(e)
         "There was an issue updating the temps."
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Replace with proper authentication
+    creds = Creds.query.first()
+    if request.form.get('user') == creds.user:
+        if request.form.get('password') == creds.pw:
+            user = User()
+            user.id = 'your_id'
+            login_user(user)
+            return redirect(url_for('fermpi'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
-    # in terminal, if port not open: flask run --host=0.0.0.0 --port=5000 --debug
-    # run_simple('0.0.0.0', port=5000, application=app)
-    db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
+    # in terminal, if port not open: flask run --host=0.0.0.0 --port=5000 --debug
 
 # TODO: get it all to work on the server
 # TODO: set file ownership on production server
-
---
+++ init_db.py
@@ -0,0 +1,46 @@
+import json
+import os.path
+
+from app import db, TempSet, Creds, app
+
+
+def init_db():
+    app.app_context().push()
+    if not os.path.isfile("./instance/temperatures.db"):
+        db.create_all()
+        print(f"DB CREATED")
+    else:
+        print("DB ALREADY EXISTS")
+
+
+def populate_db():
+    # init table TempSet
+    if not TempSet.query.count() > 0:
+        try:
+            init_temps = TempSet(temp_set=18, th_set=1, th_outer=5)
+            db.session.add(init_temps)
+            db.session.commit()
+            print("INIT TEMPS CREATED")
+        except Exception as e:
+            print(e)
+    else:
+        print("TEMPS ALREADY EXIST")
+
+    # init table Creds
+    if not Creds.query.count() > 0:
+        with open('./static/user.json') as f:
+            data = json.load(f)
+            try:
+                user = Creds(user=data['user'], pw=data['pw'])
+                db.session.add(user)
+                db.session.commit()
+                print("INIT CREDS CREATED")
+            except Exception as e:
+                print(e)
+    else:
+        print("CREDS ALREADY EXISTS")
+
+
+if __name__ == "__main__":
+    init_db()
+    populate_db()
